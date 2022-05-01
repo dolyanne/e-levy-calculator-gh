9402560a1674081a63df996416032ee1e898c29b
Dorothy Opoku <dorothyopoku@Dorothys-MacBook-Air.local>
2022-05-01 18:45:23 +0000

    complete basic logic for elevy calculator

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,24 +1,61 @@
 import { h } from "preact";
+import { useState } from "preact/hooks";
 import style from "./style.css";
 
 const Home = () => {
-  const handleCheck = () => {
+  const elevyTax = 0.015; //1.5/100
+  const [amount, setAmount] = useState(0);
+  const [transferAmount, setTransferAmount] = useState(0);
+  const [elevyAmount, setElevyAmount] = useState(0);
+
+  const handleCheck = (event) => {
+    event.preventDefault();
+    //assuming it's first transfer of the day
+    if (amount > 100) {
+      console.log(amount);
+      let taxableAmount = amount - 100;
+      let elevyCharge = taxableAmount * elevyTax;
+      let totalTransferAmount = amount + elevyCharge;
+      console.log(elevyCharge);
+      setTransferAmount(totalTransferAmount);
+      setElevyAmount(elevyCharge);
+    } else {
+      setTransferAmount(amount);
+      setElevyAmount(0);
+    }
+
     console.log("checking");
+    console.log("customer amount", amount);
+  };
+
+  const handleChange = (event) => {
+    setAmount(parseInt(event.target.value));
   };
   return (
     <div class={style.home}>
       <div class={style.flexMain}>
-        <form onSubmit={handleCheck}>
+        <form onSubmit={handleCheck} method="POST">
           <div class={style.flexRow}>
             <div class={style.flexItem}>
               <label>
-                Amount: <input type="number" name="amount" />
+                Amount (GHS):{" "}
+                <input
+                  type="number"
+                  name="amount"
+                  value={amount}
+                  onChange={handleChange}
+                />
               </label>
             </div>
             <div class={style.flexItem}>
               <label>
-                Total due:
-                <input type="number" name="total_charge" />
+                Total due (GHS):
+                <input
+                  type="text"
+                  value={`${transferAmount.toFixed(2)}`}
+                  readOnly
+                  name="transferAmount"
+                />
               </label>
             </div>
           </div>
@@ -31,7 +68,7 @@ const Home = () => {
               margin: "10px",
             }}
           >
-            E-Levy: 15GHS
+            E-Levy (GHS): {`${elevyAmount.toFixed(2)}`}
           </div>
           <div
             style={{
