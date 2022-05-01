76ec7a5cf76beaaae0954dc9e5f6876fd9052995
Dorothy Opoku <dorothyopoku@Dorothys-MacBook-Air.local>
2022-05-01 17:14:57 +0000

    Build first draft for UI

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,11 +1,52 @@
-import { h } from 'preact';
-import style from './style.css';
+import { h } from "preact";
+import style from "./style.css";
 
-const Home = () => (
-	<div class={style.home}>
-		<h1>Home</h1>
-		<p>This is the Home component.</p>
-	</div>
-);
+const Home = () => {
+  const handleCheck = () => {
+    console.log("checking");
+  };
+  return (
+    <div class={style.home}>
+      <div class={style.flexMain}>
+        <form onSubmit={handleCheck}>
+          <div class={style.flexRow}>
+            <div class={style.flexItem}>
+              <label>
+                Amount: <input type="number" name="amount" />
+              </label>
+            </div>
+            <div class={style.flexItem}>
+              <label>
+                Total due:
+                <input type="number" name="total_charge" />
+              </label>
+            </div>
+          </div>
+
+          <div
+            style={{
+              justifyContent: "center",
+              alignContent: "center",
+              display: "flex",
+              margin: "10px",
+            }}
+          >
+            E-Levy: 15GHS
+          </div>
+          <div
+            style={{
+              justifyContent: "center",
+              alignContent: "center",
+              display: "flex",
+              margin: "10px",
+            }}
+          >
+            <button type="submit">Check E-Levy</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
