67338eb591147ce1207bde2bc15ad3555adbbd67
Dorothy Opoku <stephensdolyan@gmail.com>
2022-05-11 06:40:12 +0000

    add mtn cap amount to plaftform calculations

--- src/utils/platform-charges.js
+++ src/utils/platform-charges.js
@@ -1,7 +1,8 @@
 const MTN_TO_MTN_EXEMPT = 100;
 const MTN_TO_MTN_CHARGE = 0.75;
-const MTN_TO_OTHERS_EXEMPT = 50;
+const MTN_TO_OTHERS_EXEMPT = 50;// change to 100 cedis as per sara's screen shot and run test again
 const MTN_TO_OTHERS_CHARGE = 0.75;
+const MTN_ABOVE_1000_CAP = 1000;// above 1000 has a flat fee of 7.5gh
 
 export function mtnMomoTariffs({ source, destination, amount }) {
   if (destination === source) {
@@ -9,6 +10,11 @@ export function mtnMomoTariffs({ source, destination, amount }) {
     if (amount > MTN_TO_MTN_EXEMPT) {
       charge = (amount * MTN_TO_MTN_CHARGE) / 100;
     }
+    if(amount >= MTN_ABOVE_1000_CAP){
+      charge = 7.5
+
+    }
+    
     return {
       charge,
       exempt: MTN_TO_MTN_EXEMPT,
@@ -18,6 +24,10 @@ export function mtnMomoTariffs({ source, destination, amount }) {
     let charge = 0;
     if (amount > MTN_TO_OTHERS_EXEMPT) {
       charge = (amount * MTN_TO_OTHERS_CHARGE) / 100;
+    }
+    if(amount >= MTN_ABOVE_1000_CAP){
+      charge = 7.5
+
     }
     return {
       charge,
