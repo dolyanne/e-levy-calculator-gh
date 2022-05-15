8d5622089621732e93cca6c8b005ff979e157c4b
Dorothy Opoku <stephensdolyan@gmail.com>
2022-05-15 14:48:21 +0000

    Update disclaimer text

--- src/components/global/Disclaimer.js
+++ src/components/global/Disclaimer.js
@@ -3,8 +3,8 @@ import { h } from "preact";
 const Disclaimer = () => {
   return (
     <span className="disclaimer">
-      Disclaimer: We did our best to provide accurate results, however you may
-      experience differences in real-world usage, since Information on
+      Disclaimer: We did our best to provide accurate results however, you may
+      experience differences in real-world usage since Information on
       Telco and GHIPPS charges are inconsistent.
     </span>
   );
