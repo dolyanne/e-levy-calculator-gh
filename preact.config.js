eb04849b035af3bebc88e033f0250da24aff5441
Tonyeli Tay <taytonyeli@gmail.com>
2026-02-08 13:51:09 +0000

    build: revert to github deploy

--- preact.config.js
+++ preact.config.js
@@ -1,10 +1,10 @@
 export default {
     webpack(config, env, helpers, options) {
   
-        // config.output = config.output || {};
-        // if (process.env.NODE_ENV === 'production') {
-            // config.output.publicPath = "/e-levy-calculator-gh/";
-        // }
+        config.output = config.output || {};
+        if (process.env.NODE_ENV === 'production') {
+            config.output.publicPath = "/e-levy-calculator-gh/";
+        }
   
     },
   };
